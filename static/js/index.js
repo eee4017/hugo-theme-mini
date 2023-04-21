1860946573ccd80fafeaacf9ceb3f2b58d63040a
Frank Lin <eee4017@gmail.com>
2023-04-22 02:38:22 +0800

    Modify to frank style

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,20 @@
-(function() {
-    let lastScrollTop = 0;
-    const header = document.querySelector('header.profile');
+(function () {
+  let lastScrollTop = 0;
+  const header = document.querySelector('.profile'); 
+  
+  window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    window.addEventListener('scroll', () => {
-      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      if (scrollTop > lastScrollTop) {
-        header.classList.add('hide');
-      } else {
-        header.classList.remove('hide');
-      }
-      lastScrollTop = scrollTop;
-    });
-  })();
+    if (scrollTop > lastScrollTop) {
+      // Scrolling down
+      header.classList.remove('nav-up');
+      header.classList.add('nav-down');
+    } else {
+      // Scrolling up
+      header.classList.remove('nav-down');
+      header.classList.add('nav-up');
+    }
+    
+    lastScrollTop = scrollTop;
+  });
+})();
